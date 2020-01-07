9fc484483caa6b0a0cb7502cf3be20c55362590e
theeramet0793 <theerametmetha@gmail.com>
2020-01-07 14:57:35 +0700

    commit-1

--- lab5_1.cpp
+++ lab5_1.cpp
@@ -7,15 +7,26 @@ int main(){
 	cin >> N;
 	
 	string name[N];
-	int age[N];
-	cout << "Name of student = ";
-	cout << "Age of student = ";
+	int age[N],i;
+	for(i = 0 ; i < N ; i++){
+		cout << "Name of student = ";
+		cin>>name[i];
+	    cout << "Age of student = ";
+		cin>>age[i];
+
+	}
+	
 	
 	int key;
 	cout << "--------------------------------------\n";
 	cout << "Enter an age to search: ";
 	cin >> key;
 	cout << "--------------------------------------\n";
+	for ( i = 0; i < N; i++)
+	{
+	  if(key==age[i])
+	  cout<<name[i]<<endl;
+	}
 	
 
 	
